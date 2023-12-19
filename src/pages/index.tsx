e1e9c9b3e9dc144f538f347b110ea5cc1289cbf8
Nebbishy <audricciel@gmail.com>
2023-12-19 02:56:14 -0600

    setup vercel postgres database, connect with front end to display basic data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Navbar } from '../components/navbar';
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
+import prisma from '../../lib/prisma';
 
-export default function Home(): JSX.Element {
+
+export const getStaticProps: GetStaticProps = async () => {
+    const food = await prisma.foodInfo.findMany({
+        where: { dateServed: "Thursday, December 14, 2023" },
+    });
+    return {
+        props: { food },
+        revalidate: 10,
+    };
+};
+
+export default function Home({ food }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
     return (
         <div>
             <Navbar />
-            <p className="text-4xl text-center">Hello World</p>
+            <p className="text-4xl text-center">Serving Today</p>
+            <ul>
+                {food.map((food_item: any) => (
+                <li key={food_item.id}>Food name: {food_item.name} Protein: {food_item.protein}</li>
+                ))}
+            </ul>
         </div>
     );
 }
