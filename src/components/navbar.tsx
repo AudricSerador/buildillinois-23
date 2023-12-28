0ce887982aabe5c04fa3c54e751b209255e8fe4f
Nebbishy <audricciel@gmail.com>
2023-12-28 03:18:08 -0600

    Refactor navbar and pages for mobile, refined homepage layout

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,35 +1,92 @@
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 export function Navbar(): JSX.Element {
-    return (
-      <nav className="flex justify-between bg-gray-900 text-white w-screen shadow-lg">
-        <div className="px-5 xl:px-12 py-6 flex w-full items-center justify-center">
-          <Link href="/" className="text-3xl font-bold font-heading text-uiucorange absolute left-5">
-            UIUC Dining Buddy
-          </Link>
-          <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-            <li>
-              <Link href="/" className="hover:text-uiucorange">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="/allfood" className="hover:text-uiucorange">
-                All Food
-              </Link>
-            </li>
-            <li>
-              <Link href="/" className="hover:text-uiucorange">
-                My Favorites
-              </Link>
-            </li>
-            <li>
-              <Link href="/" className="hover:text-uiucorange">
-                Dining Halls
-              </Link>
-            </li>
-          </ul>
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const checkMobile = () => {
+      const mobile = window.innerWidth <= 768;
+      setIsMobile(mobile);
+      if (mobile) {
+        document.body.style.paddingTop = '64px'; // Adjust this value to match the height of your navbar
+      } else {
+        document.body.style.paddingTop = '0px';
+      }
+    };
+  
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+  
+    return () => {
+      window.removeEventListener('resize', checkMobile);
+    };
+  }, []);
+  
+  return (
+    <nav className={`${isMobile ? 'fixed top-0' : ''} bg-gray-900 text-white w-full shadow-lg z-10`}>
+      <div className="px-5 xl:px-12 py-6 flex w-full items-center justify-center">
+        <Link href="/" className="text-3xl font-bold font-heading text-uiucorange absolute left-5">
+          UIUC Dining Buddy
+        </Link>
+          {isMobile ? (
+            <>
+              <button onClick={toggleMenu} className="ml-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-white">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+              </button>
+              <div className={`fixed top-16 right-0 h-full bg-gray-900 text-white shadow-lg w-64 transition-transform duration-500 ease-out ${isOpen ? 'transform translate-x-0' : 'transform translate-x-full'}`}>
+                <div className="font-custombold flex flex-col px-5 xl:px-12 py-3">
+                  <Link href="/" className="mt-4 text-white hover:uiucorange" onClick={closeMenu}>
+                    Home
+                  </Link>
+                  <Link href="/allfood" className="mt-4 text-white hover:uiucorange" onClick={closeMenu}>
+                    All Food
+                  </Link>
+                  <Link href="/" className="mt-4 text-white hover:uiucorange" onClick={closeMenu}>
+                    My Favorites
+                  </Link>
+                  <Link href="/" className="mt-4 text-white hover:uiucorange" onClick={closeMenu}>
+                    Dining Halls
+                  </Link>
+                </div>
+              </div>
+            </>
+          ) : (
+            <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+              <li>
+                <Link href="/" className="hover:text-uiucorange">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href="/allfood" className="hover:text-uiucorange">
+                  All Food
+                </Link>
+              </li>
+              <li>
+                <Link href="/" className="hover:text-uiucorange">
+                  My Favorites
+                </Link>
+              </li>
+              <li>
+                <Link href="/" className="hover:text-uiucorange">
+                  Dining Halls
+                </Link>
+              </li>
+            </ul>
+          )}
         </div>
       </nav>
-    );
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,34 +3,31 @@ import Link from 'next/link';
 
 export default function Home(): JSX.Element {
     return (
-        <>
-            <div className="flex justify-center items-start mt-20 text-center px-4 sm:px-0">
-                <p className="text-uiucblue font-custombold text-6xl">
-                    Find your favorite dining 
-                    <br />
-                    hall food in <i>seconds</i>.
-                </p>
-            </div>
-            <div className="mt-4 justify-center items-start text-center px-4 sm:px-0">
-                <div className="container mx-auto max-w-2xl">
-                    <p className="font-custom text-xl">
-                        Tired of going to dining halls and finding nothing you like? 
-                        Annoyed at having to check the menus of every dining hall to find something you want to eat? Find food that YOU want to eat with Dining Buddy. Our platform allows you to easily search for your favorite items across all UIUC dining halls and provides you with recommendations based on your dietary preferences.
+        <div style={{height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'stretch', margin: 0, padding: 0, marginBottom: '-25px'}}>
+            <div className="m-0 p-0 bg-cover bg-center text-white flex-grow" style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/images/dininghall.jpg)`}}>
+                <div className="flex justify-center items-start mt-20 text-center px-4 sm:px-0">
+                    <p className="text-uiucorange font-custombold text-6xl">
+                        Find your favorite dining 
+                        <br />
+                        hall food in <i>seconds</i>.
                     </p>
                 </div>
+                <div className="mt-4 justify-center items-start text-center px-4 sm:px-0">
+                    <div className="container mx-auto max-w-2xl">
+                        <p className="font-custom text-xl">
+                            Tired of going to dining halls and finding nothing you like? 
+                            Annoyed at having to check the menus of every dining hall to find something you want to eat? Find food that YOU want to eat with Dining Buddy. Search for your favorite items across all UIUC dining halls and get personalized recommendations based on your dietary preferences.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex justify-center mt-8 px-4 sm:px-0">
+                    <Link href="#">
+                        <button className="bg-uiucblue text-white font-custombold py-2 px-4 rounded mr-4">
+                            Sign Up
+                        </button>
+                    </Link>
+                </div>
             </div>
-            <div className="flex justify-center mt-8 px-4 sm:px-0">
-                <Link href="#">
-                    <button className="bg-uiucblue text-white font-custombold py-2 px-4 rounded mr-4">
-                        Sign Up
-                    </button>
-                </Link>
-                <Link href="/allfood">
-                    <button className="bg-uiucblue text-white font-custombold py-2 px-4 rounded">
-                        View All Food
-                    </button>
-                </Link>
-            </div>
-        </>
+        </div>
     );
-};
+}
