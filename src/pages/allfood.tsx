eb74eeeafa7eed71e086ed711661586be0f110b0
Nebbishy <audricciel@gmail.com>
2023-12-30 17:10:27 -0600

    Moved 'allfood' fetching to API endpoint for client-side rendering

--- src/pages/allfood.tsx
+++ src/pages/allfood.tsx
@@ -1,82 +1,8 @@
-import React, { useState } from 'react';
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import React, { useState, useEffect } from 'react';
 import { FoodItemDisplay } from '../components/food_item_display';
-import prisma from '../../lib/prisma';
 import { useRouter } from 'next/router';
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const pageSize = 10;
-  const pageNumber = context.query.page ? parseInt(context.query.page as string) - 1 : 0;
-  const sortField = context.query.sortField as string;
-  const sortOrder = context.query.sortOrder as string;
-  const diningHall = context.query.diningHall as string;
-  const mealType = context.query.mealType as string;
-  const searchTerm = context.query.searchTerm as string;
-
-  const food = await prisma.foodInfo.findMany({
-    skip: pageNumber * pageSize,
-    take: pageSize,
-    orderBy: sortField && sortField !== 'undefined' ? { [sortField]: sortOrder === 'desc' ? 'desc' : 'asc' } : { id: 'asc' },
-    where: {
-      AND: [
-        diningHall && diningHall !== 'undefined' ? {
-          mealEntries: {
-            some: {
-              diningHall: { equals: diningHall },
-            },
-          },
-        } : {},
-        mealType && mealType !== 'undefined' ? {
-          mealEntries: {
-            some: {
-              mealType: { in: mealType === 'A la Carte' ? ['A la Carte--APP DISPLAY', 'A la Carte--POS FEED'] : [mealType] },
-            },
-          },
-        } : {},
-        searchTerm && searchTerm !== 'undefined' ? {
-          name: { contains: searchTerm, mode: 'insensitive'},
-        } : {},
-      ],
-    },
-    include: {
-      mealEntries: true,
-    },
-  });
-  
-  const foodCount = await prisma.foodInfo.count({
-    where: {
-      AND: [
-        diningHall && diningHall !== 'undefined' ? {
-          mealEntries: {
-            some: {
-              diningHall: { equals: diningHall },
-            },
-          },
-        } : {},
-        mealType && mealType !== 'undefined' ? {
-          mealEntries: {
-            some: {
-              mealType: mealType === 'A la Carte' ? { startsWith: 'A la Carte' } : { equals: mealType },
-            },
-          },
-        } : {},
-        searchTerm && searchTerm !== 'undefined' ? {
-          name: { contains: searchTerm, mode: 'insensitive'},
-        } : {},
-      ],
-    },
-  });
-  
-  const diningHalls = await prisma.mealDetails.groupBy({
-    by: ['diningHall'],
-  });
-
-  return {
-    props: { food, foodCount, diningHalls },
-  };
-};
-
-export default function AllFood({ food, foodCount }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
+export default function AllFood(): JSX.Element {
   const router = useRouter();
   const pageSize = 10;
   const pageNumber = router.query.page ? parseInt(router.query.page as string) : 1;
@@ -85,13 +11,44 @@ export default function AllFood({ food, foodCount }: InferGetServerSidePropsType
   const [diningHall, setDiningHall] = useState(router.query.diningHall as string);
   const [mealType, setMealType] = useState(router.query.mealType as string);
   const [searchTerm, setSearchTerm] = useState(router.query.searchTerm as string);
+  const [isLoading, setIsLoading] = useState(false);
+  const [food, setFood] = useState([]);
+  const [foodCount, setFoodCount] = useState(0);
+  const [error, setError] = useState(null);
 
-  const handlePageChange = (page: number) => {
-    router.push(`/allfood?page=${page}&sortField=${sortField}&sortOrder=${sortOrder}&diningHall=${diningHall}&mealType=${mealType}&searchTerm=${searchTerm}`);
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(`/api/get_data?page=${pageNumber}&sortField=${sortField}&sortOrder=${sortOrder}&diningHall=${diningHall}&mealType=${mealType}&searchTerm=${searchTerm}`);
+        if (!res.ok) {
+          throw Error(res.statusText);
+        }
+        const data = await res.json();
+        setFood(data.food);
+            setFoodCount(data.foodCount);
+            setIsLoading(false);
+          } catch (error) {
+            setError(error.message);
+            setIsLoading(false);
+          }
+        };
   
-  const handleFilterChange = () => {
-    router.push(`/allfood?page=1&sortField=${sortField}&sortOrder=${sortOrder}&diningHall=${diningHall}&mealType=${mealType}&searchTerm=${searchTerm}`);
+    fetchData();
+  }, [pageNumber, sortField, sortOrder, diningHall, mealType, searchTerm]);
+  
+  const handlePageChange = (newPageNumber: number) => {
+    router.push({
+      pathname: '/allfood',
+      query: {
+        page: newPageNumber,
+        sortField: sortField,
+        sortOrder: sortOrder,
+        diningHall: diningHall,
+        mealType: mealType,
+        searchTerm: searchTerm
+      }
+    });
   };
   
   const totalPages = Math.ceil(foodCount / pageSize);
@@ -145,45 +102,71 @@ export default function AllFood({ food, foodCount }: InferGetServerSidePropsType
           <option value="Beverages">Beverages</option>
           <option value="Condiments">Condiments</option>
         </select>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-custombold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleFilterChange}>Apply Filters</button>
       </div>
       
       <p className="text-4xl font-custombold mt-4">All Food ({foodCount})</p>
       <div style={{ paddingBottom: '1rem', borderBottom: '4px solid black', marginBottom: '1rem' }}></div>
-      <ul>
-        {food.length > 0 ? (
-          food.map((foodItem: any) => (
-            <FoodItemDisplay key={foodItem.id} foodItem={foodItem} />
-          ))
-        ) : (
-          <p className="font-custom text-center my-6">No results found. Please try again with a different filter query.</p>
-        )}
-      </ul>
-      <div className="flex justify-center space-x-2">
-        <button 
-          onClick={() => handlePageChange(pageNumber - 1)}
-          className={`px-4 py-2 rounded-md text-white font-custom ${pageNumber === 1 ? 'bg-gray-300 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700'}`}
-          disabled={pageNumber === 1}
-        >
-          Back
-        </button>
-        {[...Array(endPage + 1 - startPage)].map((e, i) => (
-          <button 
-            key={i} 
-            onClick={() => handlePageChange(i + startPage)}
-            className={`px-4 py-2 rounded-md text-white font-custom ${pageNumber === i+startPage ? 'bg-blue-500' : 'bg-gray-300'} hover:bg-blue-700`}
+      {isLoading ? (
+        <div role="status" className="flex justify-center items-center h-full">
+          <svg
+            aria-hidden="true"
+            className="w-16 h-16 text-uiucblue animate-spin dark:text-uiucblue fill-uiucorange"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            {i + startPage}
-          </button>
-        ))}
-        <button 
-          onClick={() => handlePageChange(pageNumber + 1)}
-          className={`px-4 py-2 rounded-md text-white font-custom ${pageNumber === totalPages ? 'bg-gray-300 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700'}`}
-          disabled={pageNumber === totalPages}
-        >
-          Next
-        </button>
-      </div>
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      
+      ) : error ? (
+        <p className="font-custom text-center my-6">Error loading dining hall data: {error}</p>
+      ) : (
+          <>
+            <ul>
+              {food.length > 0 ? (
+                food.map((foodItem: any) => (
+                  <FoodItemDisplay key={foodItem.id} foodItem={foodItem} />
+                ))
+              ) : (
+                <p className="font-custom text-center my-6">No results found. Please try again with a different filter query.</p>
+              )}
+            </ul>
+            <div className="flex justify-center space-x-2">
+              <button 
+                onClick={() => handlePageChange(pageNumber - 1)}
+                className={`px-4 py-2 rounded-md text-white font-custom ${pageNumber === 1 ? 'bg-gray-300 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700'}`}
+                disabled={pageNumber === 1}
+              >
+                Back
+              </button>
+              {[...Array(endPage + 1 - startPage)].map((e, i) => (
+                <button 
+                  key={i} 
+                  onClick={() => handlePageChange(i + startPage)}
+                  className={`px-4 py-2 rounded-md text-white font-custom ${pageNumber === i+startPage ? 'bg-blue-500' : 'bg-gray-300'} hover:bg-blue-700`}
+                >
+                  {i + startPage}
+                </button>
+              ))}
+              <button 
+                onClick={() => handlePageChange(pageNumber + 1)}
+                className={`px-4 py-2 rounded-md text-white font-custom ${pageNumber === totalPages ? 'bg-gray-300 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700'}`}
+                disabled={pageNumber === totalPages}
+              >
+                Next
+              </button>
+            </div>
+          </>
+        )}
     </div>
   );
 }
---
+++ src/pages/api/get_data.ts
@@ -0,0 +1,72 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../lib/prisma';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const pageSize = 10;
+  const pageNumber = req.query.page ? parseInt(req.query.page as string) - 1 : 0;
+  const sortField = req.query.sortField as string;
+  const sortOrder = req.query.sortOrder as string;
+  const diningHall = req.query.diningHall as string;
+  const mealType = req.query.mealType as string;
+  const searchTerm = req.query.searchTerm as string;
+
+  const food = await prisma.foodInfo.findMany({
+    skip: pageNumber * pageSize,
+    take: pageSize,
+    orderBy: sortField && sortField !== 'undefined' ? { [sortField]: sortOrder === 'desc' ? 'desc' : 'asc' } : { id: 'asc' },
+    where: {
+      AND: [
+        diningHall && diningHall !== 'undefined' ? {
+          mealEntries: {
+            some: {
+              diningHall: { equals: diningHall },
+            },
+          },
+        } : {},
+        mealType && mealType !== 'undefined' ? {
+          mealEntries: {
+            some: {
+              mealType: { in: mealType === 'A la Carte' ? ['A la Carte--APP DISPLAY', 'A la Carte--POS FEED'] : [mealType] },
+            },
+          },
+        } : {},
+        searchTerm && searchTerm !== 'undefined' ? {
+          name: { contains: searchTerm, mode: 'insensitive'},
+        } : {},
+      ],
+    },
+    include: {
+      mealEntries: true,
+    },
+  });
+  
+  const foodCount = await prisma.foodInfo.count({
+    where: {
+      AND: [
+        diningHall && diningHall !== 'undefined' ? {
+          mealEntries: {
+            some: {
+              diningHall: { equals: diningHall },
+            },
+          },
+        } : {},
+        mealType && mealType !== 'undefined' ? {
+          mealEntries: {
+            some: {
+              mealType: mealType === 'A la Carte' ? { startsWith: 'A la Carte' } : { equals: mealType },
+            },
+          },
+        } : {},
+        searchTerm && searchTerm !== 'undefined' ? {
+          name: { contains: searchTerm, mode: 'insensitive'},
+        } : {},
+      ],
+    },
+  });
+
+  const diningHalls = await prisma.mealDetails.groupBy({
+    by: ['diningHall'],
+  });
+
+  res.status(200).json({ food, foodCount, diningHalls });
+}
