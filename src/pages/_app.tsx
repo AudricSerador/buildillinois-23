38b69f951093cc2d45a59e2b5bc8a4b83c5167fd
Nebbishy <audricciel@gmail.com>
2024-01-15 19:13:46 -0600

    Added feedback form + fixed filter ordering/formatting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,19 @@ import type { AppProps } from "next/app";
 import { Navbar } from "../components/layout/navbar";
 import { Footer } from "../components/layout/footer";
 import { SEO } from "../components/layout/seo";
+import FeedbackBanner from "@/components/layout/feedback";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const showFeedbackBanner = router.pathname !== "/";
+  const [isFeedbackBannerClosed, setFeedbackBannerClosed] = useState(false);
+
+  const handleCloseFeedbackBanner = () => {
+    setFeedbackBannerClosed(true);
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       <SEO
@@ -13,7 +24,18 @@ export default function App({ Component, pageProps }: AppProps) {
       />
       <main className="flex-grow">
         <Navbar />
-        <div className="h-16 md:block hidden"></div>
+        <div
+          className={
+            !showFeedbackBanner
+              ? "h-16 md:block hidden"
+              : isFeedbackBannerClosed
+              ? "h-16 md:block hidden"
+              : "h-32 md:block hidden"
+          }
+        ></div>
+        {showFeedbackBanner && !isFeedbackBannerClosed && (
+          <FeedbackBanner onClose={handleCloseFeedbackBanner} />
+        )}
         <Component {...pageProps} />;
       </main>
       <Footer />
