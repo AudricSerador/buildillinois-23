58e58cec5373f50c0b4ac0aafdcc58f30899c02c
Audric Serador <audricciel@gmail.com>
2024-03-16 02:30:11 -0500

    fix env for supabase deploy

--- src/auth/supabase_client.ts
+++ src/auth/supabase_client.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 if (!supabaseUrl || !supabaseKey) {
   console.error('Error: Missing Supabase URL or key. Please check your environment variables.');
